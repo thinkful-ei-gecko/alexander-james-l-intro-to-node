9bc8badd7cca493698f7588bfa4a47f34aaff444
Alexander Fukui <alexander.fukui@gmail.com>
2019-09-23 10:33:42 -0500

    solved

--- buggy-script.js
+++ buggy-script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 
@@ -25,7 +27,7 @@ linesInSource.forEach(line => {
   // write the file and it's contents
   fs.writeFileSync(
     newFilePath,
-    contentArr,
+    contentArr.join(' '),
     { flag: 'w+', encoding: 'utf-8' }
   );
 });
